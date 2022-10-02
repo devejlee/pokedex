24e5c8fcadbc1b02a31a5d2381e23b1fcd0fee46
Elijah Lee <develijahlee@gmail.com>
2022-10-02 14:22:49 +0900

    refactor: add pageParam to fetchPokemon

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
-export const fetchPokemon = async () => {
+export const fetchPokemon = async ({
+  pageParam = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
+}) => {
   try {
-    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
+    const { data } = await axios.get(pageParam)
     return {results: data.results, nextPage: data.next}
   } catch (err) {
     console.error(err)
