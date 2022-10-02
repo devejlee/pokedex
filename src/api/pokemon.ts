86c7be73aad4816894fb2225be6f292cce0b9484
Elijah Lee <develijahlee@gmail.com>
2022-10-02 11:15:05 +0900

    feat: add useInfiniteQuery to map pokemons

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const fetchPokemon = async () => {
   try {
     const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
-    return data.results
+    return {results: data.results, nextPage: data.next}
   } catch (err) {
     console.error(err)
   }
--- src/hooks/useQueryPokemon.ts
+++ src/hooks/useQueryPokemon.ts
@@ -1,6 +1,8 @@
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import { fetchPokemon } from "@api/pokemon";
 
 export const useQueryPokemon = () => {
-  return useQuery(["getPokemon"], fetchPokemon);
+  return useInfiniteQuery(["pokemon"], fetchPokemon, {
+    getNextPageParam: (lastPage) => lastPage?.nextPage,
+  });
 }
