5602c345be2547fd0e0eef2931fe21fd9f7c648e
Elijah Lee <develijahlee@gmail.com>
2022-11-12 17:51:13 +0900

    fix: fix PokemonInfo styles

--- src/components/PokemonInfo/PokemonInfo.tsx
+++ src/components/PokemonInfo/PokemonInfo.tsx
@@ -30,7 +30,7 @@ const PokemonInfo = ({ isLoading, isError, name, id, image, weight, xp, abilitie
   }
 
   return (
-    <Flex bg="white" h={400} overflowY="scroll" justifyContent={'center'} alignItems={'center'} {...props}>
+    <Flex bg="#222" h={400} overflowY="scroll" justifyContent={'center'} alignItems={'center'} rounded="10px" {...props}>
       <Flex flexDir={'column'} alignItems={'center'} w="100%" maxW={300} mx="auto" border='4px' borderColor='gray.200' rounded="10px" overflow={'hidden'}>
         <Flex bg="#e0d494" flexDir={'column'} w="100%" alignItems={'center'}>
           <Text textStyle="heading2">{capitalizeFirstLetter(name)}</Text>
@@ -38,17 +38,17 @@ const PokemonInfo = ({ isLoading, isError, name, id, image, weight, xp, abilitie
             <Image src={image} layout="fill" alt={id}></Image>
           </Box>
         </Flex>
-        <HStack textStyle={'body2'} py="10px" bg="#f0eee6" borderColor='gray.200' borderBottom={'2px'}>
+        <HStack w="100%" textStyle={'body2'} py="10px" bg="#f0eee6" borderColor='gray.200' borderBottom={'2px'}>
           <Text pl="20px" w="146px">Weight</Text>
           <Text w="146px">{weight / 10} kg</Text>
         </HStack>
-        <HStack textStyle={'body2'} py="10px" bg="#f0eee6" borderColor='gray.200' borderBottom={'2px'}>
+        <HStack w="100%" textStyle={'body2'} py="10px" bg="#f0eee6" borderColor='gray.200' borderBottom={'2px'}>
           <Text pl="20px" w="146px">Xp</Text>
           <Text w="146px">{xp}</Text>
         </HStack>
         <HStack w="100%" textStyle={'body2'} py="10px" bg="#f0eee6">
           <Text pl="15px" w="146px">Abilities</Text>
-          <UnorderedList>
+          <UnorderedList w="146px">
             {abilities.map((ability) => (
               <ListItem key={ability}>{ability}</ListItem>
             ))}
